47f0c5b0590acf1191fc16ecdf8a4ee39514e27f
Jubin <jubinjohny123@gmail.com>
2021-11-21 14:16:38 -0400

    contact hbs

--- routes/contact.js
+++ routes/contact.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/',(req,res,next) => {
-  res.render('contact');
+  res.render('contact',{ employment: 'To apply for a job with my team, please feel free to send me your Linkedin profile link or CV to : '
+});
 });
 
 module.exports = router;
