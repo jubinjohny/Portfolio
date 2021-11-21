44cc1c3a59b6c4b0fe26783378ead440e9123c76
Jubin <jubinjohny123@gmail.com>
2021-11-21 14:12:42 -0400

    myprojects hbs

--- routes/project.js
+++ routes/project.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res,next) => {
-    res.render('Projects/index');
+    res.render('Projects/index',{ myprojecttext: 'Here are some projects I have worked on recently. Want to see more?'
+   });
 });
 
 module.exports = router
