e55cbaeb466193b8c5b724f7a4af165b9134d5f1
Jubin <jubinjohny123@gmail.com>
2021-11-21 13:55:57 -0400

    handlebar on about

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About this Page', 
-  pageText:'Here is some info about this Project',
+  aboutme1:'My name is Jubin Johny. I am professional Web Designer and Web Developer. Since beginning my journey as a freelance designer nearly 2 years ago, I have done remote work for agencies, consulted for start-ups, and collaborated with talented people to create digital products for both business and consumer use.',
   var:'This about my page'
  });
 });
