a665a561ebba32ed8375e09ec6ca49bbbbdd3317
Jubin <jubinjohny123@gmail.com>
2022-02-27 21:09:48 -0400

    Yearly maintenance

--- routes/project.js
+++ routes/project.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res,next) => {
-    res.render('Projects/index',{ myprojecttext: 'Here are some projects I have worked on recently. Want to see more?'
+    res.render('Projects/index',{ myprojecttext: 'Here are some projects I have worked on recently. Want to see more? '
    });
 });
 
